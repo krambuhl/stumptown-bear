1949658a6c8d70f2656bb211c41098e251879329
Evan Krambuhl <thekrambuhl@gmail.com>
2014-06-21 01:18:21 -0700

    navdeck interaction, using switchjs

--- proto/source/app/modules/switch.js
+++ proto/source/app/modules/switch.js
@@ -3,8 +3,8 @@ var Struck = require('struck');
 
 module.exports = Struck.View.extend({
 	ui: {
-		controls: '.state-control',
-		sections: '.state-section'
+		controls: '.switch-control',
+		contents: '.switch-content'
 	},
 
 	setup: function () {
@@ -13,33 +13,35 @@ module.exports = Struck.View.extend({
 		this.scopeUI();
 
 		this.ui.controls.on('click', function (ev) {
-			self.setState($(this).attr('data-state'));
+			var $this = $(this),
+				state = $this.attr('data-switchto') || $this.attr('data-switch');
+			self.setState(state);
 		});
 
-		var name = this.$el.attr('data-state') || "default";
+		var name = this.$el.attr('data-switch') || "default";
 		this.setState(name);
 	},
 
 	scopeUI: function () {
-		var stateId = this.$el.attr('data-state-id');
+		var stateId = this.$el.attr('data-switch-id');
 		if (stateId !== undefined) {
 			this.ui.controls =
-				this.ui.controls.filter('[data-state-id="' + stateId + '"]');
+				this.ui.controls.filter('[data-switch-id="' + stateId + '"]');
 
-			this.ui.sections =
-				this.ui.sections.filter('[data-state-id="' + stateId + '"]');
+			this.ui.contents =
+				this.ui.contents.filter('[data-switch-id="' + stateId + '"]');
 		}
 	},
 
 	setState: function (name) {
-		var filter = '[data-state="' + name + '"]';
+		var filter = '[data-switch="' + name + '"]';
 
-		this.$el.attr('data-state', name);
+		this.$el.attr('data-switch', name);
 
 		this.ui.controls.removeClass('is-active')
 			.filter(filter).addClass('is-active');
 
-		this.ui.sections.removeClass('is-active')
+		this.ui.contents.removeClass('is-active')
 			.filter(filter).addClass('is-active');
 	}
 });
